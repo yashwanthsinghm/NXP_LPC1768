f402c65be8431027aa28fec257a699dffe41d895
Yashwanth Singh M <yashwanthsinghm@gmail.com>
2022-03-07 18:10:36 +0530

    CAN was modified

--- 17-CAN/CANTX/can_char.d
+++
@@ -1,5 +0,0 @@
-can_char.o: can_char.c
-can_char.o: C:\Keil\ARM\INC\NXP\LPC17xx\LPC17xx.h
-can_char.o: C:\Keil\ARM\RV31\INC\core_cm3.h
-can_char.o: C:\Keil\ARM\RV31\INC\stdint.h
-can_char.o: C:\Keil\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
--- 17-CAN/CANTX/can_switch.d
+++
@@ -1,5 +0,0 @@
-can_switch.o: can_switch.c
-can_switch.o: C:\Keil\ARM\INC\NXP\LPC17xx\LPC17xx.h
-can_switch.o: C:\Keil\ARM\RV31\INC\core_cm3.h
-can_switch.o: C:\Keil\ARM\RV31\INC\stdint.h
-can_switch.o: C:\Keil\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
